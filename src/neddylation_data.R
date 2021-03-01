808fea04e77e366f02f805bcab228a3df7a37a3e
zslastman <zslastman@gmail.com>
2021-03-01 14:24:53 +0100

    neddlyation simpler

--- src/neddylation_data.R
+++ src/neddylation_data.R
@@ -15,50 +15,83 @@
 # - load the neddylation data
 # - process it so it matches our fold changes
 
-library(tidyverse)
-library(data.table)
-library(ggExtra)
-
-library(here)
-dened_protfile <- here('ext_data/dened_proteinGroups.txt')
-if(!exists(dened_protfile)){
-    dir.create(showWarn=F,here('ext_data'))
-    download.file('ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2021/02/PXD022812/proteinGroups.txt',destfile = dened_protfile)
-}
-normalizePath(dened_protfile,mustWork=TRUE)
-
-deneddata <- fread(dened_protfile)
-selcols <- deneddata %>% colnames %>% str_subset(neg=TRUE,'counts|Razor|Unique')
-selcols %>%paste0(col='\n')%>%message
-
-# So it looks like we have 4 samples here, the MG_1 and 2, and NEDP1 and 2. I'll assume these are the proteotoxic and NEDD8 KO datasets respectively. Since there's no control group to compare to (it seems), we can just averag eintensities in our 
+library(tidyverse);
+library(data.table);
+# library(ggExtra)
+library(magrittr);
 
 # +
-options(repr.plot.width=21, repr.plot.height=7)
-inner_q <- function(v,q=0.99){
-    trim = (1 - q)/2
-    (v >= quantile(v,trim)) & (v <= quantile(v,1-trim))
-}
-deneddata<-deneddata%>%mutate_at(vars(matches('Intensity')),as.numeric)
-library(naniar)
-library(ggExtra)
+library(here)
+#readxl::read_xlsx('../ext_data/lobatogil_etal_2021_s1.xlsx',sheet=3)
+mg132_gnms = suppressMessages(readxl::read_xlsx('../ext_data/lobatogil_etal_2021_s1.xlsx',sheet=3))[[2]]%>%.[!is.na(.)]
+nedd8ko_gnms = suppressMessages(readxl::read_xlsx('../ext_data/lobatogil_etal_2021_s1.xlsx',sheet=3))[[5]]
 
-rmeanintens = deneddata%>%select(matches('Intensity.*_'))%>%as.matrix%>%rowMeans
-qplot(deneddata$Intensity,rmeanintens)+ggtitle('the intensity column is just the mean of the other 4')
+length(mg132_gnms)
+length(nedd8ko_gnms)
 
+mg132_gnms%>%tail
+nedd8ko_gnms%>%tail
 
+lobato_gnms = bind_rows(.id='lobato_set', mg132=tibble(gnm=mg132_gnms), nedd8ko = tibble(gnm=nedd8ko_gnms))
+tail(lobato_gnms)
+#readxl::excel_sheets('../ext_data/lobatogil_etal_2021_s1.xlsx',sheet=3)
 # -
+
+# So it looks like we have 4 samples here, the MG_1 and 2, and NEDP1 and 2. I'll assume these are the proteotoxic and NEDD8 KO datasets respectively. Since there's no control group to compare to (it seems), we can just averag eintensities in our 
 
 # ## Harmonize gene identifiers
 #
 # We'll use a file downloaded from the biomart website to map our human gene names -> human ids -> mouse ids -> mouse names
 #
 
-deneddata$`Gene names`%>%head
-fread(here('ext_data/human_mouse_orthologs_biomart.txt.gz'))%>%head(3)
-deneddata%>%head
+# +
+orthodf = fread(here('ext_data/human_mouse_orthologs_biomart.txt.gz'))
+hgnmdf = fread('../ext_data/human_gid_gnm_biomart.txt.gz')
 
-#
+hgnmdf = hgnmdf%>%gather(src,gnm,`Gene Synonym`:`Gene name`)%>%distinct(`Gene stable ID`,`gnm`)
+
+#hgnmdf%<>%rename('gnm':=`Gene name`)
+# hgnmdf%>%head
+
+message('genes for which we can get the ID from lobatos gene names')
+message('ned8 genes')
+table(nedd8ko_gnms %in% hgnmdf$gnm)
+message('mg132 genes')
+table(mg132_gnms %in% hgnmdf$gnm)
+
+lobato_gnm_orth = lobato_gnms%>%inner_join(hgnmdf,'gnm')%>%left_join(orthodf,by="Gene stable ID")
+# lobato_gnm_orth%>%head
+lobato_gnm_orth%<>%rename('mgid':=`Mouse gene stable ID`)
+
+
+message('for how many of our combined lobato gene names do we  find an ortholog?')
+lobato_gnm_orth%>%group_by(gnm)%>%summarise(noorth=any(mgid!=''))%>%.$noorth%>%table
+
+# -
+
+# # Now we load up our fold changes as well
+
+resultslist <- readRDS(here('data/resultslist.rds'))
+mrna_gids = resultslist[[1]]$gene_id
+message('ow many of these genes have at least one ortholog appearing our annotation?')
+lobato_gnm_orth%>%group_by(gnm)%>%summarise(inmrna_dat = any(mgid %in% mrna_gids))%>%.$inmrna_dat%>%table
+
+
+# +
+options(jupyter.plot_mimetypes = "image/svg+xml") 
+
+
+
+contrasts = names(resultslist)
+contrast = contrasts[[1]]
+
+lobato_lfc_df = resultslist[[contrast]]%>%left_join(lobato_gnm_orth,by=c('gene_id'='mgid'))%>%distinct(lobato_set,gnm,log2FoldChange)
+lobato_lfc_df%>%sample_n(4)
+
+lobato_lfc_df%>%qplot(data=.,fill=lobato_set,x=log2FoldChange,geom='histogram')+facet_grid(lobato_set~.,scale='free_y')
+
+
+# -
 
 ggpubr::ggarrange(nrow=1,plotlist=list(
     ggMarginal(deneddata%>%filter(inner_q(Intensity,.99))%>%qplot(data=.,log10(1+Intensity),log10(1+`Intensity MG_1`),geom='blank')+  geom_point(),type='histogram'),
